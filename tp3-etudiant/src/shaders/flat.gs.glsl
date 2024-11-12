801fe26ec3eaa2e30f21b33d3dc5aa6d73102c43
felix-dowl <122920539+felix-dowl@users.noreply.github.com>
2024-11-11 22:24:39 -0500

    stfcvgybhnjmkl

--- tp3-etudiant/src/shaders/flat.gs.glsl
+++ tp3-etudiant/src/shaders/flat.gs.glsl
@@ -92,7 +92,7 @@ void main()
         }
     }
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < gl_in.length; i++) {
         attribOut.texCoords = attribIn[i].texCoords;
         attribOut.ambient = ambientColor;
         attribOut.emission = mat.emission;
