f17ca534772931ce764ef88ee5907ceda8f481af
Sami-Jouamaa <sami.jouamaa-desautels@polymtl.ca>
2024-11-12 20:57:35 -0500

    ambient is somewhat okay, diffuse is good and specular is weird

--- tp3-etudiant/src/shaders/phong.fs.glsl
+++ tp3-etudiant/src/shaders/phong.fs.glsl
@@ -90,27 +90,117 @@ vec3 calculateNormalSpecular(vec3 O, vec3 N)
     return specularResult;
 }
 
+vec3 calculateSpotAmbient()
+{
+    float angle;
+    vec3 ambientResult = mat.emission + mat.ambient * lightModelAmbient;
+    for (int i = 0; i < 3; i++)
+    {
+        vec3 L = normalize(attribIn.spotDir[i]);
+        vec3 S = normalize(-lights[i].spotDirection);
+        angle = max(0.0, dot(L, S));
+        if (angle < cos(spotOpeningAngle))
+        {
+            continue;
+        }
+        else
+        {
+            if (useDirect3D)
+            {
+                float factor = smoothstep(pow(cos(spotOpeningAngle), 1.01 + spotExponent/2), cos(spotOpeningAngle), dot(attribIn.spotDir[i], lights[i].spotDirection));
+                ambientResult += mat.ambient * factor;
+            }
+            else
+            {
+                float factor = pow(dot(attribIn.spotDir[i], lights[i].spotDirection), spotExponent);
+                ambientResult += mat.ambient * factor * lights[i].ambient;
+            }
+        }
+    }
+    return ambientResult;
+}
+
+vec3 calculateSpotDiffuse(vec3 O, vec3 N)
+{
+    vec3 diffuseResult = vec3(0);
+    for (int i = 0; i < 3; i++)
+    {
+        vec3 L = normalize(attribIn.spotDir[i]);
+        float angle = max(0.0, dot(L, N));
+        if (angle < cos(spotOpeningAngle))
+        {
+            diffuseResult += mat.diffuse * lights[i].diffuse * pow(cos(angle), spotExponent);
+        }
+        else
+        {
+            continue;
+        }
+    }
+    return diffuseResult * vec3(texture(diffuseSampler, attribIn.texCoords));
+}
+
+vec3 calculateSpotNormalSpecular(vec3 O, vec3 N)
+{
+    vec3 specularResult = vec3(0);
+    for (int i = 0; i < 3; i++)
+    {
+        vec3 L = normalize(attribIn.spotDir[i]);
+        vec3 D = normalize(lights[i].spotDirection);
+        // something like that, everything is black though
+        if (dot(L, D) < cos(spotOpeningAngle))
+        {
+            continue;
+        }
+        else
+        {
+            if (useBlinn)
+            {
+                float spec = max(0.0, dot(normalize(L + O), N));
+                specularResult += mat.specular * lights[i].specular * pow(spec, mat.shininess);
+            }
+            else
+            {
+                float spec = max(0.0, dot(reflect(-L, N), O));
+                specularResult += mat.specular * lights[i].specular * pow(spec, mat.shininess);
+            }
+        // }
+    }
+    return specularResult * vec3(texture(specularSampler, attribIn.texCoords));
+}
+
 void main()
 {
     // TODO
     vec3 O = normalize(attribIn.obsPos);
     vec3 N = normalize(attribIn.normal);
-    vec3 L0 = normalize(attribIn.lightDir[0]);
-    vec3 L1 = normalize(attribIn.lightDir[1]);
-    vec3 L2 = normalize(attribIn.lightDir[2]);
-
-    vec4 texture = texture2D(diffuseSampler, attribIn.texCoords);
-    vec3 ambientTemp = calculateAmbientTemp();
-    vec3 diffuseTemp = calculateDiffuseTemp(N);
+    vec4 texture = texture(diffuseSampler, attribIn.texCoords);
     vec3 specularTemp = vec3(0);
-    if (useBlinn)
+    if (useSpotlight)
     {
-        specularTemp = calculateSpecularBlinn(O, N);
+        vec3 L0 = normalize(attribIn.spotDir[0]);
+        vec3 L1 = normalize(attribIn.spotDir[1]);
+        vec3 L2 = normalize(attribIn.spotDir[2]);
+        vec3 ambientTemp = calculateSpotAmbient();
+        vec3 diffuseTemp = calculateSpotDiffuse(O, N);
+        specularTemp = calculateSpotNormalSpecular(O, N);
+        // FragColor = vec4(ambientTemp + diffuseTemp + specularTemp, 1);
+        FragColor = vec4(specularTemp, 1);
     }
     else
     {
-        specularTemp = calculateNormalSpecular(O, N);
+        vec3 ambientTemp = calculateAmbientTemp();
+        vec3 diffuseTemp = calculateDiffuseTemp(N);
+        if (useBlinn)
+        {
+            specularTemp = calculateSpecularBlinn(O, N);
+        }
+        else
+        {
+            specularTemp = calculateNormalSpecular(O, N);
+        }
+
+        FragColor = vec4(ambientTemp + diffuseTemp + specularTemp, 1);
+
     }
 
-    FragColor = vec4(ambientTemp + diffuseTemp + specularTemp, 1);
 }
