9f6c1022a4ebac89030913bb9e0b674d9836f1d7
felix-dowl <122920539+felix-dowl@users.noreply.github.com>
2024-11-12 22:32:39 -0500

    failing spot on flat

--- tp3-etudiant/src/shaders/flat.gs.glsl
+++ tp3-etudiant/src/shaders/flat.gs.glsl
@@ -92,14 +92,15 @@ void main()
         vec3 lightDir = normalize((view * vec4(light.position, 1)).xyz - center);
         float spotFactor = 1.0;
         if (useSpotlight) {
-            float cosGamma = dot(lightDir, normalize(light.spotDirection));
+            float cosGamma = dot(-lightDir, normalize(light.spotDirection));
             cosGamma = clamp(cosGamma, -1.0, 1.0); 
             float maxCos = cos(radians(spotOpeningAngle));
             if (cosGamma < maxCos) {
                 spotFactor = 0.0;
             } else {
                 if (useDirect3D) {
-
+                    float cosInner = 0.95;
+                    spotFactor = smoothstep(maxCos, cosInner, cosGamma);
                 } else {
                     spotFactor = pow(cosGamma, spotExponent);
                 }
